aaf765494a5b7da48b59a60f6d7e653adecce240
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-09-29 23:13:48 -0500

    build

---
+++ precache-manifest.87712bb8b69207bc899c13de4204297c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79966f90d3f345c0097d038175fe1d6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3ab5e7cc394016db8f",
+    "url": "/static/css/2.731ea6ec.chunk.css"
+  },
+  {
+    "revision": "5f71f2ff283174bb4466",
+    "url": "/static/css/main.50e8bff4.chunk.css"
+  },
+  {
+    "revision": "ed3ab5e7cc394016db8f",
+    "url": "/static/js/2.5dbc520d.chunk.js"
+  },
+  {
+    "revision": "5f71f2ff283174bb4466",
+    "url": "/static/js/main.fc273a8c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b9e8c781c0d3807652a75c86cd330ca",
+    "url": "/static/media/Spongeboy_Me_Bob.4b9e8c78.woff"
+  },
+  {
+    "revision": "9d1ffd87116a26f508a0ddd7f7d76b6e",
+    "url": "/static/media/Spongeboy_Me_Bob.9d1ffd87.eot"
+  },
+  {
+    "revision": "c6b49dd5b752bfe4db8f4ee8138f9933",
+    "url": "/static/media/Spongeboy_Me_Bob.c6b49dd5.ttf"
+  },
+  {
+    "revision": "f31f73469d07434f240afc191153675a",
+    "url": "/static/media/Spongeboy_Me_Bob.f31f7346.svg"
+  }
+]);
